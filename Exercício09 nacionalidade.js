0b648ba87b8505b22f21b3cde6f0e3a5361fd0eb
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2022-04-16 23:48:01 -0300

    Comparação de informações

---
+++ Exercício09 nacionalidade.js
@@ -0,0 +1,17 @@
+var nacionalidade = prompt("Digite seu pais de origem:")
+
+
+var pais= prompt("Digite o nome do País no qual você vive:")
+
+console.log(`Vivendo no País ${pais}`)
+
+if(pais == nacionalidade ){
+  console.log(`Você é NATIVO!`)
+}
+else{
+  console.log(`você é ESTRANGEIRO!`)
+}
+
+
+
+

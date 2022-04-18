ea73bcdc6092460fbdad57d980f4f6094db8e654
Everton Cordeiro <99029999+EvertonCordeiro1994@users.noreply.github.com>
2022-04-17 23:34:58 -0300

    Praticando If e Else

---
+++ Exercício da explicação de if e else.js
@@ -0,0 +1,13 @@
+var idade = 15
+
+
+if(idade < 18 && idade >= 16)
+   console.log(`você é menor de idade, mas pode votar`)
+else{
+  if(idade >=18){
+    console.log(`Você é obrigado votar`)
+    }
+   else{
+      console.log(`você não tem idade para votar.`)
+     }
+}
